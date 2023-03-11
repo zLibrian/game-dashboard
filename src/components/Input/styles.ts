4ae3b46fb43ef27be50d09c0d2b35952c0c77be2
Kevin Willian <kevinwillian084@gmail.com>
2023-03-10 23:53:00 -0300

    feat: :sparkles: add the styles to the Input component

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const StyledInputContainer = styled.label`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 0.25rem;
+  padding: 0.5rem;
+  width: 100%;
+  max-width: 20rem;
+
+  background-color: ${({ theme }) => theme.colors.gray};
+`;
+
+export const StyledInput = styled.input`
+  background-color: transparent;
+  border: none;
+  margin-left: 0.5rem;
+  width: 100%;
+
+  color: ${({ theme }) => theme.colors.white};
+  font-size: ${({ theme }) => theme.fontSizes.medium};
+`;
