260b460770ffa03b29c81f1a4e59f9f31ae237b2
Kevin Willian <kevinwillian084@gmail.com>
2023-03-21 19:24:14 -0300

    feat: :sparkles: add styles to MostPlayed component

---
+++ src/components/MostPlayed/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const StyledMostPlayed = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  grid-area: 'most-played';
+`;
+
+export const StyledGameList = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
